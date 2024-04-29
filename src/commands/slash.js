c80c2009a75b288f35657e702ad09dcb8bbcb1a7
Dan Nawara <dan@pinmonkey.com>
2024-04-29 11:29:19 -0500

    wip: getting organized

--- src/commands/slash.js
+++ src/commands/slash.js
@@ -1,9 +1,11 @@
 export const slashCommand = async ({ command, ack, say }) => {
   await ack();
-  console.log("command", command);
   switch (command.text) {
     case "bar":
       await say("I think a good dive bar would be nice");
+      break;
+    case "foo":
+      await say("bar");
       break;
     default:
       await say(`I'm not sure what to do with "${command.text}"`);
