60adbeac5874da01bc56d951be97707258a96fbe
dan@leapinc.co <Dan Nawara>
2024-06-24 12:10:42 -0500

    wip: slash command changes

--- src/commands/slash.js
+++ src/commands/slash.js
@@ -2,16 +2,22 @@ import { getOpenStores } from "../services/portalApi.js";
 
 export const slashCommand = async ({  ack, command, say }) => {
   await ack();
+  console.log({text: command.text});
   switch (command.text) {
     case "bar":
       await say("I think a good dive bar would be nice");
       break;
     case "stores":
       const {data} = await getOpenStores();
+      console.log({data});
       await say(`getting a list of stores`);
       break;
     case "foo":
       await say("bar");
+      break;
+    case '':
+    case "":
+      await say(`Gonna need a little more information there bucko`);
       break;
     default:
       await say(`I'm not sure what to do with "${command.text}"`);
